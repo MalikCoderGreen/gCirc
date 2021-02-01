445bbf0fa8e14654c1caf05a5bdb0aaafe3f0f36
Malik Green <malikgreen@Maliks-MBP.fios-router.home>
2021-01-31 22:13:16 -0500

    added test words and evenly placed squares

--- gCirc_Proj/static/js/oneEdit.js
+++ gCirc_Proj/static/js/oneEdit.js
@@ -1,22 +1,82 @@
 
 // One Edit Game code. 
+var gameWords = {
+    0: 'Affect',
+    1: 'Effect',
+    2: 'Access', 
+    3: 'Assess',
+    4: 'Altar', 
+    5: 'Alter',
+}
 
-// Create canvas game area. 
-var myGameArea = {
-    canvas: document.createElement("canvas"), 
+// Create squares for canvas. 
+function makeSquares(){
 
-    start: function(){
-        this.canvas.width = 250;
-        this.canvas.height = 250;
+   // Get game canvas area from HTML.
+    var myGameArea = document.getElementById("myCanvas");
+    var yPos1 = 0, yPos2 = 0; // Will be left square position and right square position.
+    wordSquareList = [] // List to hold all word squares.
+    
+    // Loop to draw all squares to the game canvas. 
+    for(var i = 0; i < 6; i++){
+        
+        // Squares on the left side of the canvas.
+        if(i < 3){    
 
-        this.context = this.canvas.getContext("2d");
-        document.body.appendChild(this.canvas);
+            var wordSquare = myGameArea.getContext("2d");
+            wordSquare.beginPath();
+            wordSquare.strokeStyle = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+            wordSquare.rect(0, yPos1, myGameArea.width / 2, myGameArea.height / 3);
+            wordSquare.stroke();
+            wordSquareList.push(wordSquare);
+            yPos1 += 200;
+
+    }
+      // Squares on the right side of the canvas. 
+      else if(i >= 3){
+            
+            var wordSquare = myGameArea.getContext("2d");
+            wordSquare.beginPath();
+            wordSquare.strokeStyle = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+            wordSquare.rect(200, yPos2, myGameArea.width / 2, myGameArea.height / 3);
+            wordSquare.stroke();
+            wordSquareList.push(wordSquare);
+            yPos2 += 200;        
+      }
+      
     }
 
 
+    // Set style for all words in list. 
+    wordSquareList[0].font = 'italic 900 16px Russo One';
+    
+    // Loop that will print all words into the squares of the screen.
+    var lPos = 100, rPos = 100;
+    for(var i = 0; i < wordSquareList.length; i++){
+        if(i < 3){
+            wordSquareList[i].fillText(gameWords[i], 100, lPos);
+            lPos += 200;
+        }
+
+        else{
+            wordSquareList[i].fillText(gameWords[i], 300, rPos)
+            rPos += 200;
+
+        }
+
+    }
+
+
+}
+
+// Hide start button after it is pressed. 
+function hide(){
+    var div = document.getElementById('gButtonContainer');
+    div.style.display = 'none';
 
 }
 
 function startGame(){
-    myGameArea.start();
+    makeSquares();
 }
+
